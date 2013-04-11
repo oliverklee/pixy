392e3fd4bd9e481df0c6af2218f0b2cca7da6989
Oliver Klee <typo3-coding@oliverklee.de>
2013-04-12 00:19:00 +0200

    [CLEANUP] Delete unused testfiles/backup-depgraph.

--- testfiles/backup-depgraph/test85.php
+++
@@ -1,9 +0,0 @@
-<? //
-
-// a called method is defined in an included file (was a bug)
-
-include 'test85b.php';
-$foo->bar($evil, 'ok');
-
-
-?>
--- testfiles/backup-depgraph/test90.php
+++
@@ -1,9 +0,0 @@
-<? //
-
-// was a bug that led to an exception
-
-$f->g->gaga();
-
-
-
-?>
