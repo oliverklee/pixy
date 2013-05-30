4f83ef8ceba6856c5cfaef040812a309d3c27180
Oliver Klee <typo3-coding@oliverklee.de>
2013-05-30 14:43:00 +0200

    [TASK] Add an example PHP file plus parse tree diagram.

--- example/example.php
+++ example/example.php
@@ -1,7 +1,20 @@
 <?php
-$untainted = 1;
-echo $untainted;
+class Foo {
+  var $taintedField;
+  var $untaintedField;
+}
 
-$tainted = $_GET['x'];
-echo $tainted;
+$untaintedVariable = 42;
+echo $untaintedVariable;
+
+$taintedVariable = $_GET['x'];
+echo $taintedVariable;
+
+$foo = new Foo();
+
+$foo->untaintedField = 'Hello world';
+echo $foo->untaintedField;
+
+$foo->taintedField = $_GET['x'];
+echo $foo->taintedField;
 ?>
